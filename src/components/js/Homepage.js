31aa5efbc4233bc04f9a8047dd702f1d824f2f28
joannechen1223 <b04701232@ntu.edu.tw>
2019-03-13 01:56:34 +0800

    finish education part

--- src/components/js/Homepage.js
+++ src/components/js/Homepage.js
@@ -9,7 +9,7 @@ class Homepage extends Component {
       <div className="homepage">
         <div className="self-intro">
           <p className="title">JOU-AN (JOANNE) CHEN</p>
-          <div className="subtitle"> Senior Student of <b>National Taiwan University</b><br/>Double Major in <b>Business Administration</b> and <b>Computer Science</b></div>
+          <div className="subtitle"><b>Always Prepared to be amazed.</b></div>
           <div className="content">Hi! I am Joanne, a college student as well as a self learner in web development. I am interested in the industries of technology and finance, and looking forward to new challenges in different fields.</div>
           <div className="contact">
             <button className="contactButton">CONTACT ME</button>
@@ -24,27 +24,48 @@ class Homepage extends Component {
             <p className="remark" id="time">09/2015 ~ 06/2020 (expected)</p>
             <p className="remark" id="city">Taipei, Taiwan</p>
             <hr />
-            <p className="subtitle" id="relevant-coursework">Relevant Coursework</p>
+            <div className="subtitle" id="relevant-coursework">Relevant Coursework</div>
             <div className="course" id="ba-related">
-              <img
-                className="course-icon"
-                src={require('../img/ba.png')}
-                alt="BA course icon"
-              />
+              <div className="course-icon-div">
+                <img
+                    className="course-icon"
+                    src={require('../img/ba.png')}
+                    alt="BA course icon"
+                />
+              </div>
+              <div className="course-content">
+                <p className="course-related-tile">BA Related</p>
+                <p className="course-name">Finacial Accounting, Marketing Management, Operating Management, Cost Accounting, Project Management, Strategic Management</p>
+              </div>
+              <div style={{clear: "both"}}></div>
             </div>
             <div className="course" id="cs-related">
-              <img
-                className="course-icon"
-                src={require('../img/cs.png')}
-                alt="CS course icon"
-              />
+              <div className="course-icon-div">
+                <img
+                    className="course-icon"
+                    src={require('../img/cs.png')}
+                    alt="CS course icon"
+                />
+              </div>
+              <div className="course-content">
+                <p className="course-related-tile">CS Related</p>
+                <p className="course-name">Data Structure, Algorithm Design and Analysis, System Programming, Operating System, Computer Network, Computer Architecture, Machine Learning, Database Management</p>
+              </div>
+              <div style={{clear: "both"}}></div>
             </div>
             <div className="course" id="math-related">
-              <img
-                className="course-icon"
-                src={require('../img/math.png')}
-                alt="MATH course icon"
-              />
+              <div className="course-icon-div">
+                <img
+                    className="course-icon"
+                    src={require('../img/math.png')}
+                    alt="MATH course icon"
+                />
+              </div>
+              <div className="course-content">
+                <p className="course-related-tile">MATH & STATISTIC Related</p>
+                <p className="course-name">Calculus, Linear Algebra, Statistic and Probability, Discrete Mathematics</p>
+              </div>
+              <div style={{clear: "both"}}></div>
             </div>            
           </div>
         </div>
